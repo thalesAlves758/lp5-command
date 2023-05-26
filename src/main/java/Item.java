376bd4431199534328c8fe94f87105e574983259
thalesAlves758 <thalesalves758@gmail.com>
2023-05-25 21:16:03 -0300

    feat: create Item class

---
+++ src/main/java/Item.java
@@ -0,0 +1,17 @@
+public class Item {
+    private int code;
+    private String name;
+
+    public Item(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
